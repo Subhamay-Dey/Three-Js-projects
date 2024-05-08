9dfaa637b5aae84c39257777e85a85bc7e443deb
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-09 02:24:43 +0530

    finally my 1st three js project is done. ohh yeahhh~

--- threejs-project1/src/components/Overplay.jsx
+++ threejs-project1/src/components/Overplay.jsx
@@ -37,7 +37,7 @@ export const Overlay = () => {
 
   return (
     <Scroll html>
-      <div class="w-screen">
+      <div class="w-screen ">
         <Section opacity={opacityFirstSection}>
           <h1 className="font-semibold font-serif text-2xl">
             Hello, I'm Subhamay Dey
@@ -52,44 +52,51 @@ export const Overlay = () => {
           <p className="animate-bounce  mt-6">↓</p>
         </Section>
         <Section right opacity={opacitySecondSection}>
-          <h1 className="font-semibold font-serif text-2xl">
+          <h1 className="font-semibold font-serif text-2xl select-none">
             Here are my skillsets 🔥
           </h1>
-          <p className="mt-6">
+          <p className="mt-6 select-none">
             <b>Frontend 🚀</b>
           </p>
-          <ul className="leading-9">
+          <ul className="leading-9 select-none">
             <li>ReactJS</li>
             <li>React Native</li>
             <li>Tailwind</li>
             <li>Scss & Less</li>
+            <li>GSAP</li>
             <li>ThreeJS</li>
           </ul>
-          <p className="mt-3">
+          <p className="mt-3 select-none">
             <b>Backend 🔬</b>
           </p>
-          <ul className="leading-9">
+          <ul className="leading-9 select-none">
             <li>NodeJS</li>
             <li>ExpressJS</li>
             <li>NestJS</li>
             <li>MongoDb</li>
+            <li>PassportJS</li>
             <li>Firebase</li>
           </ul>
-          <p className="animate-bounce  mt-6">↓</p>
+          <p className="animate-bounce  mt-6 select-none">↓</p>
         </Section>
         <Section opacity={opacityLastSection}>
-          <h1 className="font-semibold font-serif text-2xl">
-             U can Connect with me
+          <h1 className="font-semibold font-serif text-2xl flex select-none">
+             U can Connect with me😄
           </h1>
-          <p className="text-gray-500 mb-2">
+          <p className="text-gray-500 mb-2 pt-1 select-none">
             I may help with your projects and learning.
           </p>
-          <p className="mt-6 p-3 bg-slate-100 rounded-lg flex">
-          🌐
-          <p className="font-semibold">
-              <a href="https://www.linkedin.com/in/subhamay-dey-5b8273292/">My LinkedIn Profile</a>
-            </p>
+          <p className="mt-6 p-3 bg-slate-100 rounded-lg ">
+            <section className="flex">
+            🌐
+            <a href="https://www.linkedin.com/in/subhamay-dey-5b8273292/" className="font-bold">My LinkedIn Profile</a>
               <p className="pl-4">⬅️ Click</p>
+              </section>
+
+            <section className="flex pt-4 ">
+            📬  <p className="font-bold">ncdey1966@gmail.com</p>
+            <p className="pl-4">⬅️ gmail</p>
+            </section> 
           </p>
         </Section>
       </div>
