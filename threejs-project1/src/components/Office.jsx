de492c4b108649274095d7dac48716ac5f642e4b
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-08 20:29:55 +0530

    gsap animation is successfully implemented with useScroll, useRef, useFrame, useLayoutEffect and gsap.

--- threejs-project1/src/components/Office.jsx
+++ threejs-project1/src/components/Office.jsx
@@ -1,13 +1,49 @@
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useLayoutEffect, useRef } from 'react'
+import { useGLTF, useScroll } from '@react-three/drei'
+import { gsap } from 'gsap';
+import { useFrame } from '@react-three/fiber';
+
+const FLOOR_HEIGHT = 2.3;
+const NB_FLOORS = 3;
 
 export function Office(props) {
-  const { nodes, materials } = useGLTF('./models/WawaOffice.glb')
+  const { nodes, materials } = useGLTF('./models/WawaOffice.glb');
+  const ref = useRef();
+  const tl = useRef();
+
+  const scroll = useScroll();
+
+  useFrame(() => {
+    tl.current.seek(scroll.offset * tl.current.duration());
+  })
+
+    useLayoutEffect(() => {
+        tl.current = gsap.timeline();
+        
+        tl.current.to(
+            ref.current.position, 
+            {
+                duration: 2,
+                y: -FLOOR_HEIGHT * (NB_FLOORS - 1),
+            },
+            0
+        );
+
+    }, [])
+
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} ref={ref}>
       <mesh geometry={nodes['01_office'].geometry} material={materials['01']} />
-      <mesh geometry={nodes['02_library'].geometry} material={materials['02']} position={[0, 2.114, -2.23]} />
-      <mesh geometry={nodes['03_attic'].geometry} material={materials['03']} position={[-1.97, 4.227, -2.199]} />
+
+        <mesh geometry={nodes['02_library'].geometry} 
+            material={materials['02']} 
+            position={[0, 2.114, -2.23]} 
+        />
+
+        <mesh geometry={nodes['03_attic'].geometry} 
+            material={materials['03']} 
+            position={[-1.97, 4.227, -2.199]} 
+        />
     </group>
   )
 }
