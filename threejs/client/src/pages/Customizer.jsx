62e1b8c779cd77ebe131e33fe63df0c21cd6bc9c
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-18 05:15:14 +0530

    Updated the customizer component with the several usestate hooks and gave the generatetabContent function which will define its functions acoording to the three tabs in editor tab and also rendered it and also set the setActiveEditorTab with the specific tab name which is being clicked inside handleClick.

--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -5,7 +5,7 @@ import { useSnapshot } from 'valtio';
 
 import config from '../config/config';
 import state from '../store';
-import {download} from '../assets';
+import {download, logoShirt, stylishShirt} from '../assets';
 import {downloadCanvasToImage, reader} from '../config/helpers';
 import {EditorTabs, FilterTabs, DecalTypes} from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
@@ -15,6 +15,30 @@ import {CustomButton, AiPicker, ColorPicker, FilePicker, Tab,} from '../componen
 const Customizer = () => {
 
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker/>
+      case "filepicker":
+        return <FilePicker/>
+      case "aipicker":
+        return <AiPicker/>
+    
+      default:
+        return null;
+    }
+  }
 
   return (
     <AnimatePresence>
@@ -31,9 +55,10 @@ const Customizer = () => {
                   <Tab 
                     key= {tab.name}
                     tab= {tab}
-                    handleClick= {() => {}}
+                    handleClick= {() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
 
